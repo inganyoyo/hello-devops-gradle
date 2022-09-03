a12658576faacca9c96353422e9a4d179986b502
inganyoyo <inganyoyo@me.com>
2022-09-04 08:59:20 +0900

    k8s 파일

--- hello-springboot/src/main/java/com/example/hellospringboot/HelloController.java
+++ hello-springboot/src/main/java/com/example/hellospringboot/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String test() {
-        return "Hello World! v1.4";
+        return "Hello World! v1.5";
     }
 }
