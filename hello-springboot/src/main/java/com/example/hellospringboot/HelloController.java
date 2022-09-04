cecbb9f6072d221c29f16a387349faf91f83b586
inganyoyo <inganyoyo@me.com>
2022-09-04 22:03:17 +0900

    version 1.8

--- hello-springboot/src/main/java/com/example/hellospringboot/HelloController.java
+++ hello-springboot/src/main/java/com/example/hellospringboot/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String test() {
-        return "Hello World! v1.7";
+        return "Hello World! v1.8";
     }
 }
